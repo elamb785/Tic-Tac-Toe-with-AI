63ebe1238b11208a6ea6fc9f12cca1394aeb6125
Evan Lamb <elamb785@gmail.com>
2020-08-09 10:29:49 -0700

    add normal difficulty

--- tictactoe.py
+++ tictactoe.py
@@ -7,8 +7,22 @@
 # 1) Code requires running of is_winner twice in the case that game_over returns True (that's a lot of wasted time)
 
 import random
+from itertools import combinations
 
 class Board():
+
+    win_conditions = [
+        ['1', '2', '3'],
+        ['4', '5', '6'],
+        ['7', '8', '9'],
+
+        ['1', '4', '7'],
+        ['2', '5', '8'],
+        ['3', '6', '9'],
+
+        ['1', '5', '9'],
+        ['7', '5', '3']
+    ]
     
     def __init__(self):
         self.board = {'7': ' ', '8': ' ', '9': ' ',
@@ -40,20 +54,19 @@ class Board():
     def is_full(self):
         return True if set(self.board.values()) == {'X', 'O'} else False
 
+    def win_possible(self, player):
+        '''return where to play for a possible win'''
+        for win in self.win_conditions:
+            for i in combinations(win, 2):
+                if self.board[i[0]] == self.board[i[1]] == player.token:
+                    move_prelim = [spot for spot in win if spot not in i][0]     
+                    if self.is_open(move_prelim):
+                        return move_prelim 
+        return None
+
     def is_winner(self, player):
-        win_conditions = [
-            ['1', '2', '3'],
-            ['4', '5', '6'],
-            ['7', '8', '9'],
-
-            ['1', '4', '7'],
-            ['2', '5', '8'],
-            ['3', '6', '9'],
-
-            ['1', '5', '9'],
-            ['7', '5', '3']
-        ]
-        for i, j, k in win_conditions:
+        '''return whether someone has one. If so, who'''
+        for i, j, k in self.win_conditions:
             if self.board[i] == self.board[j] == self.board[k] == player.token:
                 return True, player.name
         return False
@@ -68,20 +81,30 @@ class Player():
         self.token = token
         self.name = name
 
-    def make_move(self, board=None):
-        return self.strategy(self, board)
+    def make_move(self, board, game):
+        return self.strategy(self, board, game)
 
 # Define the possible strategies for make_move method
 def Hard(player, board):
+    '''minmax algorithm'''
     pass
 
-def Medium(player, board):
-    pass
+def Medium(player, board, game):
+    '''Win, if possible. If not, block. Else, random.'''
+    move = board.win_possible(player)
+    if move:
+        return move
+    move = board.win_possible(game.change_turn(player))
+    if move:
+        return move
+    else:
+        return random.choice(board.open_spaces())
 
-def Easy(player, board):
+def Easy(player, board, game=None):
+    '''Play randomly'''
     return random.choice(board.open_spaces())
 
-def User(player, board):
+def User(player, board, game=None):
     move = input("Insert move (1-9): ")
     while move not in board.get_keys():
         print("Please enter a value between 1 and 9")
@@ -110,6 +133,7 @@ function_mappings = {
 }
 
 def get_players_diff():
+    '''Determine and convert to functions the inputted difficulties'''
     players = ("User", "Easy", "Medium", "Hard")
     player = []
     for i in range(1, 3):
@@ -128,7 +152,6 @@ def query_new_game():
     return True if ans == 'y' else False
 
 def start_game(p1, p2):
-    game_going = True
     p1_user = input("What is player 1's name? ")
     p2_user = input("What is player 2's name? ")
     game = Game(p1, p2, p1_user, p2_user)
@@ -140,11 +163,12 @@ def start_game(p1, p2):
     player = game.p1
     print(f'{player.name}\'s Turn')
 
-    while game_going:
-        move = str(player.make_move(game.board))
+    # initial play begins here
+    while True:
+        move = str(player.make_move(game.board, game))
         while not game.board.is_open(move):
             print("Position already occupied. Please try again.")
-            move = player.make_move(game.board)
+            move = player.make_move(game.board, game)
         game.board.update_board(move, player)
         print(f'{player.name} played at position {move}.')
         game.board.print_board()
@@ -152,12 +176,10 @@ def start_game(p1, p2):
         if game.board.game_over(player):
             if game.board.is_winner(player):
                 print(f'{player.name} wins!')
-                game_going = False
                 return query_new_game()
             else:
                 if game.board.is_full():
                     print("Tie!")
-                    game_going = False
                     return query_new_game()
         else:
             player = game.change_turn(player)
